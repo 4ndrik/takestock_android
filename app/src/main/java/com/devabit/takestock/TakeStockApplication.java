15562559ab36d3280e2c03db8a4876b855213a59
VictorArtemyev <victorartemyev@outlook.com>
2016-04-07 14:27:43 +0300

    [add] implemented TakeStockApplication

---
+++ app/src/main/java/com/devabit/takestock/TakeStockApplication.java
@@ -0,0 +1,15 @@
+package com.devabit.takestock;
+
+import android.app.Application;
+
+/**
+ * Created by Victor Artemyev on 07/04/2016.
+ */
+public class TakeStockApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+}
+
+
