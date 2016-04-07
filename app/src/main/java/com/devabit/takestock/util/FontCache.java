c6dfa2bee8788c9d6fc3b609ce662a1ef9236b40
VictorArtemyev <victorartemyev@outlook.com>
2016-04-07 18:03:24 +0300

    [add] implemented FontCache

---
+++ app/src/main/java/com/devabit/takestock/util/FontCache.java
@@ -0,0 +1,29 @@
+package com.devabit.takestock.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.StringRes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Victor Artemyev on 07/04/2016.
+ */
+public class FontCache {
+
+    private static Map<String, Typeface> sCachedFonts = new HashMap<>();
+
+    public static Typeface getTypeface(Context context, String assetPath) {
+        if (!sCachedFonts.containsKey(assetPath)) {
+            Typeface tf = Typeface.createFromAsset(context.getAssets(), assetPath);
+            sCachedFonts.put(assetPath, tf);
+        }
+
+        return sCachedFonts.get(assetPath);
+    }
+
+    public  static Typeface getTypeface(Context context, @StringRes int resId) {
+        return getTypeface(context, context.getString(resId));
+    }
+}
