51441784f02bff9d4886a67633752c6eeb5d166c
VictorArtemyev <victorartemyev@outlook.com>
2016-04-14 16:57:05 +0300

    [add] implemented OfferDialog content

---
+++ app/src/main/java/com/devabit/takestock/OfferDialog.java
@@ -0,0 +1,42 @@
+package com.devabit.takestock;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Victor Artemyev on 14/04/2016.
+ */
+public class OfferDialog extends DialogFragment {
+
+    public static OfferDialog newInstance() {
+        Bundle args = new Bundle();
+
+        OfferDialog dialog = new OfferDialog();
+        dialog.setArguments(args);
+        dialog.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+        dialog.setCancelable(true);
+        return dialog;
+    }
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_fragment_offer, container, false);
+    }
+
+    @Override public void onViewCreated(View view, Bundle savedInstanceState) {
+        ButterKnife.bind(OfferDialog.this, view);
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(OfferDialog.this);
+    }
+}
--- app/src/main/java/com/devabit/takestock/ProductDetailActivity.java
+++ app/src/main/java/com/devabit/takestock/ProductDetailActivity.java
@@ -3,8 +3,6 @@ package com.devabit.takestock;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,6 +10,7 @@ import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
@@ -37,15 +36,11 @@ public class ProductDetailActivity extends AppCompatActivity {
                         mContent.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                     }
                 });
+    }
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    @OnClick(R.id.make_offer_fab)
+    protected void onMakeOfferFabClick() {
+        OfferDialog dialog = OfferDialog.newInstance();
+        dialog.show(getFragmentManager(), dialog.getClass().getSimpleName());
     }
 }
