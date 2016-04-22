79573cf896a7e9c9745d6faaaf1d23b34255ec73
VictorArtemyev <victorartemyev@outlook.com>
2016-04-22 14:20:11 +0300

    [add] implemented Logger

--- app/src/dev/java/java/com/devabit/takestock/Injection.java
+++ app/src/dev/java/java/com/devabit/takestock/Injection.java
@@ -5,7 +5,10 @@ import android.util.Log;
 /**
  * Enables injection of production implementations for
  * {@link } at compile time.
+ *
+ * Created by Victor Artemyev on 22/04/2016.
  */
+
 public class Injection {
     public static void injectTest() {
         Log.d("Tag", "inject");
