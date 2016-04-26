7cf7eb0aadf37a9c9d621679c7eba47a8ca9fc9d
VictorArtemyev <victorartemyev@outlook.com>
2016-04-26 21:36:44 +0300

    [add] implemented UserCredentials class

---
+++ app/src/main/java/com/devabit/takestock/data/model/UserCredentials.java
@@ -0,0 +1,36 @@
+package com.devabit.takestock.data.model;
+
+import com.devabit.takestock.data.remote.RequestObject;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Victor Artemyev on 25/04/2016.
+ */
+public final class UserCredentials implements RequestObject {
+
+    private static final String NAME = "username";
+    private static final String PASSWORD = "password";
+
+    public final String name;
+    public final String password;
+
+    public UserCredentials(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    @Override public String toJsonString() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(NAME, name);
+        json.put(PASSWORD, password);
+        return json.toString();
+    }
+
+    @Override public String toString() {
+        return "UserCredentials{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
