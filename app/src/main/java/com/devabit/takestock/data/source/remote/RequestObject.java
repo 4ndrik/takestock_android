93389cbdfb527befaf06c4ce2229e8afe6f3b8c7
VictorArtemyev <victorartemyev@outlook.com>
2016-04-29 17:54:26 +0300

    [rfc] removed RequestObject

--- app/src/main/java/com/devabit/takestock/data/source/remote/RequestObject.java
+++
@@ -1,9 +0,0 @@
-package com.devabit.takestock.data.source.remote;
-
-/**
- * Created by Victor Artemyev on 26/04/2016.
- */
-public interface RequestObject {
-
-    String toJsonString() throws Exception;
-}
