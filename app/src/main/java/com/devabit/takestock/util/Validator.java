2573586d3dfd7b5b2a086b115d71af7243c00f3f
VictorArtemyev <victorartemyev@outlook.com>
2016-05-06 14:06:11 +0300

    [imp] implemented Validator

---
+++ app/src/main/java/com/devabit/takestock/util/Validator.java
@@ -0,0 +1,32 @@
+package com.devabit.takestock.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by Victor Artemyev on 06/05/2016.
+ */
+public final class Validator {
+
+    public static final Pattern USER_NAME_PATTERN = Pattern.compile("^[A-Za-z0-9\\.\\+\\-_@]{3,30}$");
+
+    public static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\._%\\-\\+]{1,256}" +
+            "@" +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+            ")+"
+            );
+
+    public static boolean validateUserName(String userName) {
+        return USER_NAME_PATTERN.matcher(userName).matches();
+    }
+
+    public static boolean validateEmailAddress(String emailAddress) {
+        return EMAIL_ADDRESS_PATTERN.matcher(emailAddress).matches();
+    }
+
+    private Validator() {
+    }
+}
