e4dbcda6f6b8a868cca8de791d712d51cc1d0969
VictorArtemyev <victorartemyev@outlook.com>
2016-09-01 17:00:53 +0300

    [add] added Timber logging

--- app/src/main/java/com/devabit/takestock/TakeStockApplication.java
+++ app/src/main/java/com/devabit/takestock/TakeStockApplication.java
@@ -1,6 +1,7 @@
 package com.devabit.takestock;
 
 import android.app.Application;
+import timber.log.Timber;
 
 /**
  * Created by Victor Artemyev on 07/04/2016.
@@ -9,6 +10,11 @@ public class TakeStockApplication extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
+        Timber.plant(new Timber.DebugTree() {
+            @Override protected String createStackElementTag(StackTraceElement element) {
+                return super.createStackElementTag(element) + ":" + element.getLineNumber();
+            }
+        });
     }
 }
 
